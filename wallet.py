8e4e1acca2399046b2a9027812b9c9f0c053e9a2
AkritW <mail2max.w@gmail.com>
2024-07-23 10:17:29 +0700

    should fail

--- wallet.py
+++ wallet.py
@@ -3,7 +3,7 @@ class Wallet:
         self.balance = balance
 
     def add_balance(self, val):
-        self.balance = self.balance + val
+        self.balance = self.balance - val
 
     def get_balance(self):
         return self.balance
